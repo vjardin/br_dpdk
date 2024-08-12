43a354845c380624966d0a330aa59d720480ae04
Vincent JARDIN <vjardin@free.fr>
2024-08-12 18:39:22 +0200

    fetch from dpdk.org instead of github
    dpdk.org is much faster for fetching the repository.

--- package/dpdk/dpdk.mk
+++ package/dpdk/dpdk.mk
@@ -6,7 +6,7 @@
 
 DPDK_VERSION = main
 # DPDK_VERSION ?= v24.03
-DPDK_SITE = $(call github,DPDK,dpdk,$(DPDK_VERSION))
+DPDK_SITE = https://dpdk.org/git/dpdk
 DPDK_SITE_METHOD = git
 DPDK_LICENSE = BSD-3-Clause
 DPDK_LICENSE_FILES = license/bsd-3-clause.txt license/README license/bsd-2-clause.txt license/exceptions.txt license/gpl-2.0.txt license/isc.txt license/lgpl-2.1.txt license/mit.txt
