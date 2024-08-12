5299982ebecce7e8ebdc3402afb92617d92704d9
Vincent JARDIN <vjardin@free.fr>
2024-08-12 18:13:26 +0200

    mk: using main instead of vmain
    WIP, do not use it yet.

--- package/dpdk/dpdk.mk
+++ package/dpdk/dpdk.mk
@@ -5,8 +5,8 @@
 #################################################################################
 
 DPDK_VERSION = main
-# DPDK_VERSION ?= 24.03
-DPDK_SITE = $(call github,DPDK,dpdk,v$(DPDK_VERSION))
+# DPDK_VERSION ?= v24.03
+DPDK_SITE = $(call github,DPDK,dpdk,$(DPDK_VERSION))
 DPDK_SITE_METHOD = git
 DPDK_LICENSE = BSD-3-Clause
 DPDK_LICENSE_FILES = license/bsd-3-clause.txt license/README license/bsd-2-clause.txt license/exceptions.txt license/gpl-2.0.txt license/isc.txt license/lgpl-2.1.txt license/mit.txt
