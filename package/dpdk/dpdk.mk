9e3a84ad8676a328f7a77584a3f213c28bfaa4f3
Vincent Jardin <vjardin@free.fr>
2024-08-19 21:45:41 +0200

    Config.in, mk: apply reviews from Thomas
    Comments here:
      https://lore.kernel.org/buildroot/20240817102923.18f56e24@windsurf/T/#raa29ba303b1405bcbbabcf1f72751312dd6dde7f
    
    Suggested-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>

--- package/dpdk/dpdk.mk
+++ package/dpdk/dpdk.mk
@@ -1,39 +1,43 @@
 ################################################################################
 #
-# DPDK
+# dpdk
 #
 ################################################################################
 
-# DPDK_VERSION = main
-# DPDK_SITE = https://dpdk.org/git/dpdk
-# DPDK_SITE_METHOD = git
-DPDK_VERSION ?= 24.03
+DPDK_VERSION = 24.03
 DPDK_SOURCE = dpdk-$(DPDK_VERSION).tar.xz
 DPDK_SITE = http://fast.dpdk.org/rel
-DPDK_LICENSE = BSD-3-Clause
-DPDK_LICENSE_FILES = license/bsd-3-clause.txt license/README license/bsd-2-clause.txt license/exceptions.txt license/gpl-2.0.txt license/isc.txt license/lgpl-2.1.txt license/mit.txt
+DPDK_LICENSE = \
+	BSD-3-Clause, \
+	BSD-2-Clause,\
+	GPL-2.0, \
+	ISC, \
+	LGPL-2.1, \
+	MIT
 
-DPDK_DEPENDENCIES += host-pkgconf
-DPDK_DEPENDENCIES += host-python-pyelftools
-DPDK_DEPENDENCIES += libbsd
+DPDK_LICENSE_FILES = \
+	license/bsd-3-clause.txt \
+	license/README \
+	license/bsd-2-clause.txt \
+	license/exceptions.txt \
+	license/gpl-2.0.txt \
+	license/isc.txt \
+	license/lgpl-2.1.txt \
+	license/mit.txt \
+
+DPDK_DEPENDENCIES = \
+	host-pkgconf \
+	host-python-pyelftools \
+	libbsd \
+	jansson \
+	libpcap \
+	zlib \
+
+ifeq ($(BR2_TOOLCHAIN_USES_GLIBC),)
 DPDK_DEPENDENCIES += libexecinfo
-DPDK_DEPENDENCIES += jansson
-DPDK_DEPENDENCIES += libpcap
-DPDK_DEPENDENCIES += zlib
-#not yet DPDK_DEPENDENCIES += openssl
-#not yet DPDK_DEPENDENCIES += libbpf
+endif
 
-DPDK_MARCH = $(BR2_ARCH)
-DPDK_MTUNE = $(BR2_ARCH) # not used yet
-GCC_TARGET_CPU=$(BR2_GCC_TARGET_ARCH)
-
-# see meson_options.txt from DPDK
-#
-DPDK_CONF_OPTS += -Ddeveloper_mode=enabled
-
-DPDK_CONF_OPTS += -Dcpu_instruction_set=$(DPDK_MARCH)
-
-# platform can be: native, all, cn9k, cn10k
-DPDK_CONF_OPTS += -Dplatform=generic
+DPDK_CONF_OPTS += -Dcpu_instruction_set=$(BR2_GCC_TARGET_ARCH)
+#DPDK_CONF_OPTS += -Dplatform=generic
 
 $(eval $(meson-package))
